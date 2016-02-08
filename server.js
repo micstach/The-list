2e40b245cd04993ae5bd4a82f196672ab7342078
micstach <micstach@poczta.onet.pl>
2016-02-09 00:05:09 +0100

    Basic setup completed

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 var express = require('express') ;
+//var mongoose = require('mongoose') ; 
+
+var port = 80 ;
+
+var app = express() ;
+
+app.set('views', __dirname + '/views');  
+app.set('view engine', 'ejs');  
+
+app.get('*', function(req, res){
+	res.render('index', {title:'Welcome page'}) ;
+}) ;
+
+app.listen(port, function(){
+	console.log('Server started, port: %s', port) ;
+}) ;
